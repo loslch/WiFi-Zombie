85309ee9ac65657801fd1b158dea4dd4ff3d7d95
ChangHun Lee <loslch@gmail.com>
2013-12-03 16:58:19 -0500

    서브모듈 정리. Protocol 추가

--- src/com/fragments/OutdoorSurveyFragment.java
+++ src/com/fragments/OutdoorSurveyFragment.java
@@ -2,7 +2,9 @@ package com.fragments;
 
 import java.util.List;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -10,9 +12,12 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.data.WifiDataItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.LocationSource;
@@ -21,20 +26,29 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.wifi_zombie.R;
 
-public class OutdoorSurveyFragment extends MyFragment implements LocationListener, LocationSource {
+public class OutdoorSurveyFragment extends MyFragment implements LocationListener, LocationSource, OnClickListener {
 	private GoogleMap mMap = null;
 	
     private OnLocationChangedListener mListener;
     private LocationManager locationManager;
+    
+    private View inflater;
+    private Button btnSaveCurrentAPs; 
+    private Context context;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.activity_map, null, false);
+		this.inflater = inflater.inflate(R.layout.activity_map, null, false);
+		return this.inflater;
 	}
 
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
+		
+		context = getActivity().getApplicationContext();
+		btnSaveCurrentAPs = (Button) inflater.findViewById(R.id.btnSaveCurrentAPs);
+		btnSaveCurrentAPs.setOnClickListener(this);
 
         locationManager = (LocationManager) 
 				getActivity().getSystemService(Context.LOCATION_SERVICE);
@@ -95,6 +109,48 @@ public class OutdoorSurveyFragment extends MyFragment implements LocationListene
 	        getFragmentManager().beginTransaction().remove(f).commit();
 	        mMap = null;
 	    }
+	}
+	
+	@Override
+	public void onClick(View view) {
+		switch (view.getId()) {
+		case R.id.btnSaveCurrentAPs:
+			WifiDataItem item = null;
+			for(int i=0 ; i<super.wifidata.getSize() ; i++)
+			{
+				if(super.wifidata.getWifiInfoData(i).isConnected())
+					item = super.wifidata.getWifiInfoData(i);
+			}
+			String result = "";
+			if(item != null)
+			{
+				result += item.getSSID();
+				result += "\nSSID : ";
+				result += item.getBSSID();
+				result += "\nMAC Addr : ";
+				result += item.getStrength();
+				result += "\nSecurity Mode : ";
+				result += item.getSecurityMode();
+				result += "\nFrequency : ";
+				result += item.getFrequency()+"Mhz";
+				result += "\nBandwidth : ";
+				result += item.getBandWidth()+"G";
+			}
+			else
+				result += "\nConnected nothing";
+    		Toast.makeText(context, result, Toast.LENGTH_SHORT).show();
+    		
+			break;
+
+		default:
+			break;
+		}		
+	}
+	
+	@Override
+	public void updateWifiData() {
+		super.updateWifiData();
+		
 	}
 
 	private void setUpMapIfNeeded() {
@@ -192,20 +248,20 @@ public class OutdoorSurveyFragment extends MyFragment implements LocationListene
    public void onProviderDisabled(String provider) 
    {
        // TODO Auto-generated method stub
-       Toast.makeText(getActivity().getApplicationContext(), "provider disabled", Toast.LENGTH_SHORT).show();
+       Toast.makeText(context, "provider disabled", Toast.LENGTH_SHORT).show();
    }
 
    @Override
    public void onProviderEnabled(String provider) 
    {
        // TODO Auto-generated method stub
-       Toast.makeText(getActivity().getApplicationContext(), "provider enabled", Toast.LENGTH_SHORT).show();
+       Toast.makeText(context, "provider enabled", Toast.LENGTH_SHORT).show();
    }
 
    @Override
    public void onStatusChanged(String provider, int status, Bundle extras) 
    {
        // TODO Auto-generated method stub
-       Toast.makeText(getActivity().getApplicationContext(), "status changed", Toast.LENGTH_SHORT).show();
+       Toast.makeText(context, "status changed", Toast.LENGTH_SHORT).show();
    }
 }
